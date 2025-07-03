0fdfc239a1a94445f972562ccef9aafb64bb8b9e
ElenaMalysheva <photo.malysheva@gmail.com>
2025-07-03 19:11:29 +0400

    Добавлено несколько функций

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,6 +1,27 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+        hello("user");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 5;
+        double b = 6;
+        System.out.println("Площадь прямоуголькина со сторонами " + a + " и "+ b + " = " + area(a, b));
+
+
+
+    }
+    public static void hello(String somebody){
+        System.out.println("Hello "+somebody+"!");
+    }
+    public static double area(double l){
+        return l*l;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
     }
 }
