688c929371471c2430b94fc1a906095ce4e130cd
ElenaMalysheva <photo.malysheva@gmail.com>
2025-07-08 16:33:52 +0400

    Создали классы и объекты

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+        Rectangle r = new Rectangle(5,6);
+        System.out.println("Площадь прямоуголькина со сторонами " + r.a + " и "+ r.b + " = " + area(r));
+
+
+
+    }
+    public static void hello(String somebody){
+        System.out.println("Hello "+somebody+"!");
+    }
+    public static double area(Square s){
+        return s.l* s.l;
+    }
+
+    public static double area (Rectangle r){
+        return r.a * r.b;
+    }
+
+}
