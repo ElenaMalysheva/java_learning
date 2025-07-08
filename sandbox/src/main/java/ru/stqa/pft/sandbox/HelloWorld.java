af6ce5372dfcff6a37a6a8add7cf33cafe77988c
ElenaMalysheva <photo.malysheva@gmail.com>
2025-07-08 16:53:53 +0400

    функции, ассоциированные с объектами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -6,10 +6,10 @@ class HelloWorld {
         hello("user");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(5,6);
-        System.out.println("Площадь прямоуголькина со сторонами " + r.a + " и "+ r.b + " = " + area(r));
+        System.out.println("Площадь прямоуголькина со сторонами " + r.a + " и "+ r.b + " = " + r.area());
 
 
 
@@ -17,12 +17,9 @@ class HelloWorld {
     public static void hello(String somebody){
         System.out.println("Hello "+somebody+"!");
     }
-    public static double area(Square s){
-        return s.l* s.l;
-    }
 
-    public static double area (Rectangle r){
-        return r.a * r.b;
-    }
+
+
+
 
 }
